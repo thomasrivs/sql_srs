61653f6c935f8940e8c2fce351c845bf788650ec
j <j>
2023-12-30 18:07:24 +0100

    correctif bug

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("Hello world")
+
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    sql_query = st.text_area(label="enter your input")
+    result = duckdb.sql(sql_query).df()
+    st.write(f'You ran the following query: {sql_query}')
+    st.dataframe(result)
+
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
