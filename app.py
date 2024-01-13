847169a4ceea6623948f7290fdf120ba92a46270
j <j>
2024-01-13 11:20:05 +0100

    add ci and cleanup code with black

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
-import streamlit as st
-import pandas as pd
-import duckdb
 import io
 
-csv = """
+import duckdb
+import pandas as pd
+import streamlit as st
+
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
-Tea,3
+Tea,3CSV
 """
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = """
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
 """
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STRING = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution = duckdb.sql(answer).df()
+solution_df = duckdb.sql(ANSWER_STRING).df()
 
 with st.sidebar:
     option = st.selectbox(
-       "What would you like to review?",
-       ("Joins", "Group By", "Windows Functions"),
-       index=None,
-       placeholder="Select a theme...",
+        "What would you like to review?",
+        ("Joins", "Group By", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme...",
     )
-    st.write('You selected:', option)
+    st.write("You selected:", option)
 
 
 st.header("enter your code:")
@@ -43,6 +44,19 @@ if query:
     result = duckdb.query(query).df()
     st.dataframe(result)
 
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+
+    except KeyError as e:
+        st.write("Some columns are missing!")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df!"
+        )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -51,8 +65,8 @@ with tab2:
     st.write("table: food_items")
     st.dataframe(food_items)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 
 with tab3:
-    st.write(answer)
+    st.write(ANSWER_STRING)
