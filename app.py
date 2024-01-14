4280b94b5b69b20247e99e3b6a3689eea20a26dc
j <j>
2024-01-14 10:45:15 +0100

    comparison funcitonnality added

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import ast
 import duckdb
 import streamlit as st
 
 con = duckdb.connect(database="data/my-db.duckdb", read_only=False)
-
-
-# solution_df = duckdb.sql(ANSWER_STRING).df()
 
 with st.sidebar:
     theme = st.selectbox(
@@ -19,27 +15,30 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}' ").df()
     st.write(exercise)
 
+    EXERCISE_NAME = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{EXERCISE_NAME}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
-
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#    try:
-#        result = result[solution_df.columns]
-#        st.dataframe(result.compare(solution_df))
-#
-#    except KeyError as e:
-#        st.write("Some columns are missing!")
-#
-#    n_lines_difference = result.shape[0] - solution_df.shape[0]
-#    if n_lines_difference != 0:
-#        st.write(
-#            f"result has a {n_lines_difference} lines difference with the solution_df!"
-#        )
-#
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        (st.write("Some columns are missing!"))
+
+n_lines_difference = result.shape[0] - solution_df.shape[0]
+if n_lines_difference != 0:
+    st.write(
+        f"result has a {n_lines_difference} lines difference with the solution_df!"
+    )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -50,7 +49,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    EXERCISE_NAME = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{EXERCISE_NAME}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
